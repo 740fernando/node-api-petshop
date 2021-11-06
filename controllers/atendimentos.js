a16380baf1c06dce9a2c7b4ab897a5fe2061485f
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-11-06 15:44:07 -0300

    Persistindo os dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports= app =>{
     app.get('/atendimentos',(req,res)=> res.send(` Você esta na rota de atendimento e está realizando um GET`));
+    
     app.post('/atendimentos',(req,res)=>{
-        console.log(req.body);
-        res.send('Voê está na rota de atendimento e está realizando um POST')
+        const atendimento = req.body;
+        Atendimento.adiciona(atendimento);
+        res.send('Post atendimento');
     });
 }
 
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas')
 
 conexao.connect(erro =>{
     if(erro){
         console.log(erro);
     }else{
-        console.log("conectado com sucesso")
+        console.log("conectado com sucesso");
+
+        Tabelas.init(conexao)//cria as tabelas
+
         const app = customExpress();
         app.listen(3000, () => console.log('servidor rodando na porta 3000'));  
     }
 })
-
 
 //responsabilidade do index é colocar o servidor execucação
