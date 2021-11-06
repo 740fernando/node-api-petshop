8693bf441a3edee8a09960ed148edf5fc4ba7452
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-11-06 20:18:47 -0300

    UPDATE

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,12 +1,28 @@
+const atendimentos = require('../models/atendimentos')
 const Atendimento = require('../models/atendimentos')
 
 module.exports= app =>{
-    app.get('/atendimentos',(req,res)=> res.send(` Você esta na rota de atendimento e está realizando um GET`));
+    app.get('/atendimentos',(req,res)=>{
+      atendimentos.lista(res)
+
+    })
+
+    app.get('/atendimentos/:id',(req,res)=>{
+        const id = parseInt(req.params.id)
+        
+        atendimentos.buscarPorId(id,res);
+    })
     
     app.post('/atendimentos',(req,res)=>{
         const atendimento = req.body;
-        Atendimento.adiciona(atendimento, res);
+        atendimentos.adiciona(atendimento, res);
     });
+
+    app.patch('/atendimentos/:id',(req,res)=>{
+      const id = parseInt(req.params.id);
+      const valores = req.body;
+      atendimentos.alterar(id,valores,res);
+    })
 }
 
 /**
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -3,16 +3,75 @@ const conexao = require('../infraestrutura/conexao')
 
 class Atendimento{
     adiciona(atendimento, res) {
-        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
-        const data = moment(atendimento.data,'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-        const atendimentoDatado= {...atendimento, dataCriacao, data};
-        const sql = 'INSERT INTO Atendimentos SET ?'
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+        const data = moment(atendimento.data,'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao); // isSameOrAfter- vai comparar se a data do body é igual ou superior a dtCriacao, retorna true caso seja
+        const clienteEhValido = atendimento.cliente.length >=5
+        console.log(dataEhValida);
+        const validacoes = [
+            {
+                nome: "data",
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome:'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
+        
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length
 
-        conexao.query(sql,atendimentoDatado,(erro,resultados)=>{
+        if(existemErros){
+            res.status(400).json(erros)
+        }else{
+            const atendimentoDatado= {...atendimento, dataCriacao, data};
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql,atendimentoDatado,(erro,resultados)=>{
+                if(erro){
+                   res.status(400).json(erro);
+                }else{
+                    res.status(201).json(resultados);
+                }
+            })
+        }
+    }
+    lista(res){
+        const sql = 'SELECT * from atendimentos'
+        conexao.query(sql, (erro, resultados) =>{
             if(erro){
-               res.status(400).json(erro);
+                res.status(400).json(erro);
             }else{
-                res.status(201).json(resultados);
+                res.status(200).json(resultados);
+            }
+        })
+    }
+    buscarPorId(id, res){
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+
+        conexao.query(sql,(erro,resultado)=>{
+            const atendimento= resultado[0];
+            if(erro){
+                res.status(400).json(erro);
+            }else{
+                res.status(200).json(atendimento);
+            }
+        })
+    }
+    alterar(id, valores, res){
+        if(valores.data){
+            valores.data = moment(valores.data,'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+        }
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?';
+
+        conexao.query(sql, [valores, id], (erro, resultados)=>{
+            if(erro){
+                res.status(400).json(erro);
+            }else{
+                res.status(200).json(resultados);
             }
         })
     }
