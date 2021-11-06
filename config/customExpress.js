22d322c807679eaf39c3acee104b11d84a255f7a
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-11-06 13:09:32 -0300

    Implementando o post

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,10 +1,14 @@
-const express = require('express'); // biblioteca
-const consign = require('consign')
-const app= express();
+const express = require('express'); //configurações comuns da aplicação web, como a porta a ser usada para conexão e a localização dos modelos que são usados para renderizar a resposta
+const consign = require('consign')//Consign é sugerida para facilitar o gerenciamento das rotas no express
+
 
 module.exports=() =>{
+    const app= express();
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true}))
     consign()
         .include('controllers')
         .into(app) // estou passando app para o controler
     return app;
 }
+//CustomExpress- configuraçoes do express
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,13 @@
 module.exports= app =>{
-    app.get('/atendimentos',(req,res)=> res.send('Você esta na rota de atendimento e está realizando um GET'));
+    app.get('/atendimentos',(req,res)=> res.send(` Você esta na rota de atendimento e está realizando um GET`));
+    app.post('/atendimentos',(req,res)=>{
+        console.log(req.body);
+        res.send('Voê está na rota de atendimento e está realizando um POST')
+    });
 }
+
+/**
+  a rota e as funcionalidades
+Quando queremos enviar uma requisição usamos get, quando queremos receber usaremos post
+req
+ **/
