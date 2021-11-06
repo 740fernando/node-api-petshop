256a35775fc868b8384e3eacb76e3136e25c0e2c
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-11-06 09:14:56 -0300

    Criado a primeira rota

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express'); // biblioteca
+
+const app= express();
+
+app.listen(3000, ()=> console.log('servidor rodando na porta 3000'));
+
+app.get('/atendimentos',(req,res)=> res.send('Você esta na rota de atendimento '));
+
