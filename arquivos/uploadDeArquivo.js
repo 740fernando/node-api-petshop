a272187a868fdc68876de31326984e647ca35446
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-11-07 23:04:27 -0300

    Rota para criar pets

--- arquivos/uploadDeArquivo.js
+++ arquivos/uploadDeArquivo.js
@@ -1,10 +1,8 @@
-const fs = require('fs');
+const fs = require('fs');// modulo file system, responsavel por manipular os arquivos 
 
-fs.readFile('./assets/pitbull.jpg',async (erro, buffer)=>{
-    console.log('imagem foi bufferizada')
-    console.log(buffer)
+//stream- vai pegar algo( no nosso caso, subir a imagem ou salvar imagem), vai fazendod isso em paralelo, conforme for precisando, nao precisa parar de executar suas coisas.  
+//createReadStream - cria uma stream de leitura.
 
-    fs.writeFile('./assets/pitbull2.jpg', buffer, erro=>{
-        console.log('imagem foi inscrita')
-    })
-})
+fs.createReadStream('./assets/pitbull.jpg')
+    .pipe(fs.createWriteStream('./assets/pitbull-stream.jpg'))//responsavel pela conversao a stream de leitura para escrita
+    .on('finish', () => console.log('Imagem foi escrita com sucesso')) // nossas streams imitem eventos, podemos configurar o metodo on() é executado, quando um evento for chamado
