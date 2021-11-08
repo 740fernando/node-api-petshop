92a535bae20bca7f0cdb68d78aee68dcd32b2e37
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-11-08 10:01:09 -0300

    Manipulando imagem no servidor

--- arquivos/uploadDeArquivo.js
+++ arquivos/uploadDeArquivo.js
@@ -3,6 +3,10 @@ const fs = require('fs');// modulo file system, responsavel por manipular os arq
 //stream- vai pegar algo( no nosso caso, subir a imagem ou salvar imagem), vai fazendod isso em paralelo, conforme for precisando, nao precisa parar de executar suas coisas.  
 //createReadStream - cria uma stream de leitura.
 
-fs.createReadStream('./assets/pitbull.jpg')
-    .pipe(fs.createWriteStream('./assets/pitbull-stream.jpg'))//responsavel pela conversao a stream de leitura para escrita
-    .on('finish', () => console.log('Imagem foi escrita com sucesso')) // nossas streams imitem eventos, podemos configurar o metodo on() é executado, quando um evento for chamado
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada)=>
+{
+    const novoCaminho= `./assets/imagens/${nomeDoArquivo}`;
+    fs.createReadStream(caminho)
+        .pipe(fs.createWriteStream(novoCaminho))//responsavel pela conversao a stream de leitura para escrita
+        .on('finish', () => callbackImagemCriada(novoCaminho)) // nossas streams imitem eventos, podemos configurar o metodo on() é executado, quando um evento for chamado
+}
